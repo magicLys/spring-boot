228aec5d0a8c00ce521c6a4949d188ba341ad597
lys0001 <359696776@qq.com>
2017-09-29 14:54:35 +0800

    demo init
    demo init

---
+++ common-web/src/main/java/com/lys/CommonWebApplication.java
@@ -0,0 +1,12 @@
+package com.lys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonWebApplication.class, args);
+	}
+}
